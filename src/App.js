686f47edf2a4394a579d2978135aecbea17a2d97
lucas-paz <lucas.hpaz@outlook.com>
2021-07-18 14:27:47 -0300

    component app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import Header from './Components/Header';
+import Hero from './Components/Hero';
+import About from './Components/About';
+import Works from './Components/Works';
+import Skills from './Components/Skills';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 import '../node_modules/bulma/css/bulma.css';
 import './App.css';
@@ -7,6 +13,12 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
+      <About />
+      <Works />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 }
