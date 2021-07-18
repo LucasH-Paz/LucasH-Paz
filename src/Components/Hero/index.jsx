b8b32e9a362a03a841d0de2c7b682687dc1c8362
lucas-paz <lucas.hpaz@outlook.com>
2021-07-18 15:47:08 -0300

    creates hero

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Particles from 'react-particles-js';
+// import Particles from 'react-particles-js';
 import './styles.css';
 
 function Hero() {
   return (
-    <div>
-      <Particles
+    <div id="hero">
+      {/* <Particles
         params={{
           "particles": {
             "number": {
-              "value": 50
+              "value": 150
             },
             "size": {
               "value": 3
@@ -18,12 +18,22 @@ function Hero() {
           "interactivity": {
             "events": {
               "onhover": {
-                "enable": true,
+                "enable": false,
                 "mode": "repulse"
               }
             }
           }
-        }} />
+        }}
+      /> */}
+      <div>
+        <h1 className="title is-1">Welcome</h1>
+        <h2 className="subtitle is-3">
+          I'm so glad in having you here, go ahead and check out some of my works.
+        </h2>
+      </div>
+      <button type="button" className="button is-large is-primary is-outlined">
+        Explore the Projects
+      </button>
     </div>
   )
 }
