84910a0ca455d368e99235b1ab7bb7491b77cb75
lucas-paz <lucas.hpaz@outlook.com>
2021-07-18 14:53:13 -0300

    installs react-particles

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import './styles.css';
 
 function Hero() {
-  return(
+  return (
     <div>
-      Hero
+      <Particles
+        params={{
+          "particles": {
+            "number": {
+              "value": 50
+            },
+            "size": {
+              "value": 3
+            }
+          },
+          "interactivity": {
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "repulse"
+              }
+            }
+          }
+        }} />
     </div>
   )
 }
